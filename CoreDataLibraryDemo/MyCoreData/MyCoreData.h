a2397eb4fdd0c5f7af30001b7d494801b5c3d28f
hanson <hulefei29@gamil.com>
2013-08-11 15:39:12 +0800

    add coredatalibrarydemo
    测试在静态类库中使用core data

---
+++ CoreDataLibraryDemo/MyCoreData/MyCoreData.h
@@ -0,0 +1,17 @@
+//
+//  MyCoreData.h
+//  MyCoreData
+//
+//  Created by Hanson on 13-8-11.
+//  Copyright (c) 2013年 Hanson. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface MyCoreData : NSObject {
+    
+}
+
++ (void)hello;
+
+@end
