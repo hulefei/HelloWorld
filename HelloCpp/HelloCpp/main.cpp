9c4278dee9f862a1d1ac5f0e5ac9b9421f56060f
hanson <hulefei29@gamil.com>
2013-09-24 18:07:19 +0800

    根据章节分类

--- HelloCpp/HelloCpp/main.cpp
+++ HelloCpp/HelloCpp/main.cpp
@@ -9,59 +9,72 @@
 #include <iostream>
 #include <cstdlib>
 
-#include "Box.h"
-#include "Carton.h"
+//#include "Box.h"
+//#include "Carton.h"
 
-void test1();
-void test2();
-void test3();
-void test4();
-double boxSurface(const Box &theBox);
+#include "Chapter7.h"
+
+//void test1();
+//void test2();
+//void test3();
+//void test4();
+//double boxSurface(const Box &theBox);
+
+void chapter7();
+
 
 int main(int argc, const char * argv[]) {
-//    test1();
-//    test2();
-//    test3();
-    test4();
-
+    chapter7();
     return 0;
 }
 
-//friend
-double boxSurface(const Box &theBox) {
-    return theBox.length;
+void chapter7() {
+    Chapter7 *chapter7 = new Chapter7();
+//    chapter7->helloworld();
+//    chapter7->method01();
+//    chapter7->method02();
+//    chapter7->method03();
+//    chapter7->method04();
+    chapter7->method05();
+    
+    delete chapter7;
 }
 
-void test4() {
-    Carton *carton = new Carton();
-    
-    
-    delete carton;
-}
-
-void test3() {
-    Box *firstBox = new Box(80.0,50.0,40.0);
-    Box secondBox(9.0,18.0,4.0);
-    
-    std::cout << boxSurface(*firstBox) << std::endl;
-    std::cout << "Volume of first Box object is " << firstBox->volume() << std::endl;;
-    
-    std::cout << "the first box is "
-    << (firstBox->compareVolume(secondBox) >= 0 ? "" : "not")
-    << "greater than the second box" << std::endl;
-    
-    delete firstBox;
-}
-
-void test1() {
-    double d = 1.332;
-    int i = static_cast<int>(d);
-    std::cout << i;
-}
-void test2() {
-    std::cout << "int size :" << sizeof(int) << std::endl;
-    std::cout << "double size :" << sizeof(double) << std::endl;
-    std::cout << "float size :" << sizeof(float) << std::endl;
-    std::cout << "long size :" << sizeof(long) << std::endl;
-}
+////friend
+//double boxSurface(const Box &theBox) {
+//    return theBox.length;
+//}
+//
+//void test4() {
+//    Carton *carton = new Carton();
+//    
+//    
+//    delete carton;
+//}
+//
+//void test3() {
+//    Box *firstBox = new Box(80.0,50.0,40.0);
+//    Box secondBox(9.0,18.0,4.0);
+//    
+//    std::cout << boxSurface(*firstBox) << std::endl;
+//    std::cout << "Volume of first Box object is " << firstBox->volume() << std::endl;;
+//    
+//    std::cout << "the first box is "
+//    << (firstBox->compareVolume(secondBox) >= 0 ? "" : "not")
+//    << "greater than the second box" << std::endl;
+//    
+//    delete firstBox;
+//}
+//
+//void test1() {
+//    double d = 1.332;
+//    int i = static_cast<int>(d);
+//    std::cout << i;
+//}
+//void test2() {
+//    std::cout << "int size :" << sizeof(int) << std::endl;
+//    std::cout << "double size :" << sizeof(double) << std::endl;
+//    std::cout << "float size :" << sizeof(float) << std::endl;
+//    std::cout << "long size :" << sizeof(long) << std::endl;
+//}
 
