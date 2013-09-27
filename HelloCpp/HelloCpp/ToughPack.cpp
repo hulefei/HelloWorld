a24148e37119163560f5ae95f6409eeefb4faada
hanson <hulefei29@gamil.com>
2013-09-27 15:01:40 +0800

    delete

--- HelloCpp/HelloCpp/ToughPack.cpp
+++
@@ -1,9 +0,0 @@
-//
-//  ToughPack.cpp
-//  HelloCpp
-//
-//  Created by Hanson on 13-9-11.
-//  Copyright (c) 2013年 Hanson. All rights reserved.
-//
-
-#include "ToughPack.h"
--- HelloCpp/HelloCpp/main.cpp
+++ HelloCpp/HelloCpp/main.cpp
@@ -9,24 +9,15 @@
 #include <iostream>
 #include <cstdlib>
 
-//#include "Box.h"
-//#include "Carton.h"
-
 #include "Chapter7.h"
-
-//void test1();
-//void test2();
-//void test3();
-//void test4();
-//double boxSurface(const Box &theBox);
 
 void chapter7();
 
 
-int main(int argc, const char * argv[]) {
-    chapter7();
-    return 0;
-}
+//int main(int argc, const char * argv[]) {
+//    chapter7();
+//    return 0;
+//}
 
 void chapter7() {
     Chapter7 *chapter7 = new Chapter7();
@@ -40,41 +31,4 @@ void chapter7() {
     delete chapter7;
 }
 
-////friend
-//double boxSurface(const Box &theBox) {
-//    return theBox.length;
-//}
-//
-//void test4() {
-//    Carton *carton = new Carton();
-//    
-//    
-//    delete carton;
-//}
-//
-//void test3() {
-//    Box *firstBox = new Box(80.0,50.0,40.0);
-//    Box secondBox(9.0,18.0,4.0);
-//    
-//    std::cout << boxSurface(*firstBox) << std::endl;
-//    std::cout << "Volume of first Box object is " << firstBox->volume() << std::endl;;
-//    
-//    std::cout << "the first box is "
-//    << (firstBox->compareVolume(secondBox) >= 0 ? "" : "not")
-//    << "greater than the second box" << std::endl;
-//    
-//    delete firstBox;
-//}
-//
-//void test1() {
-//    double d = 1.332;
-//    int i = static_cast<int>(d);
-//    std::cout << i;
-//}
-//void test2() {
-//    std::cout << "int size :" << sizeof(int) << std::endl;
-//    std::cout << "double size :" << sizeof(double) << std::endl;
-//    std::cout << "float size :" << sizeof(float) << std::endl;
-//    std::cout << "long size :" << sizeof(long) << std::endl;
-//}
 
