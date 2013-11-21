116c32ea88c18b5c397f917b571c313992549938
hanson <hulefei29@gamil.com>
2013-11-21 11:39:58 +0800

    搭建hellolua 完成

--- HelloLua/HelloLua/LuaCpp.cpp
+++
@@ -1,9 +0,0 @@
-//
-//  LuaCpp.cpp
-//  HelloLua
-//
-//  Created by Hanson on 13-10-16.
-//  Copyright (c) 2013年 Hanson. All rights reserved.
-//
-
-#include "LuaCpp.h"
--- HelloLua/HelloLua/main.cpp
+++ HelloLua/HelloLua/main.cpp
@@ -7,12 +7,21 @@
 //
 
 #include <iostream>
+#include "lua.hpp"
 
-int main(int argc, const char * argv[])
-{
-
-    // insert code here...
-    std::cout << "Hello, World!\n";
+int main(int argc, const char * argv[]) {
+    char buff[256];
+    int error;
+    lua_State *L = luaL_newstate();
+    luaL_openlibs(L);        /* opens the standard libraries */
+    while (fgets(buff, sizeof(buff), stdin) != NULL) {
+        error = luaL_loadstring(L, buff) || lua_pcall(L, 0, 0, 0);
+        if (error) {
+            fprintf(stderr, "%s\n", lua_tostring(L, -1));
+            lua_pop(L, 1);  /* pop error message from the stack */
+        }
+    }
+    lua_close(L);
     return 0;
 }
 
