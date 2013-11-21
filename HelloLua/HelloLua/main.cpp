fecbbf6a9411500b16514de9d6ab21ebdc1f2541
hanson <hulefei29@gamil.com>
2013-11-22 00:20:16 +0800

    modify HelloLua

--- HelloLua/HelloLua/main.cpp
+++ HelloLua/HelloLua/main.cpp
@@ -8,20 +8,36 @@
 
 #include <iostream>
 #include "lua.hpp"
+#include "CLuaFn.h"
 
 int main(int argc, const char * argv[]) {
-    char buff[256];
-    int error;
-    lua_State *L = luaL_newstate();
-    luaL_openlibs(L);        /* opens the standard libraries */
-    while (fgets(buff, sizeof(buff), stdin) != NULL) {
-        error = luaL_loadstring(L, buff) || lua_pcall(L, 0, 0, 0);
-        if (error) {
-            fprintf(stderr, "%s\n", lua_tostring(L, -1));
-            lua_pop(L, 1);  /* pop error message from the stack */
-        }
-    }
-    lua_close(L);
+//    char buff[256];
+//    int error;
+//    lua_State *L = luaL_newstate();
+//    luaL_openlibs(L);        /* opens the standard libraries */
+//    while (fgets(buff, sizeof(buff), stdin) != NULL) {
+//        error = luaL_loadstring(L, buff) || lua_pcall(L, 0, 0, 0);
+//        if (error) {
+//            fprintf(stderr, "%s\n", lua_tostring(L, -1));
+//            lua_pop(L, 1);  /* pop error message from the stack */
+//        }
+//    }
+//    lua_close(L);
+//    
+//    return 0;
+    
+    CLuaFn LuaFn;
+    
+    LuaFn.Init();
+    
+    LuaFn.LoadLuaFile("/Users/hanson/Documents/Workspace/GitHub/HelloWorld/HelloLua/HelloLua/Sample.lua");
+    
+    
+    LuaFn.CallFileFn("func_Add", 1, 10);
+    LuaFn.CallFileFn("func_Add", 2, 20);
+    LuaFn.CallFileFn("func_Add", 3, 30);
+    
+    getchar();
     
     return 0;
 }
