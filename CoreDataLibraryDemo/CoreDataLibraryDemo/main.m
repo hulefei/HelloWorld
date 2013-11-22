337152097816aaf6d9a9afb1d7a8483b1f86b749
hanson <hulefei29@gamil.com>
2013-11-23 00:23:44 +0800

    add CoreDataLibraryDemo

---
+++ CoreDataLibraryDemo/CoreDataLibraryDemo/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  CoreDataLibraryDemo
+//
+//  Created by Hanson on 13-8-11.
+//  Copyright (c) 2013年 Hanson. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "CDAppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([CDAppDelegate class]));
+    }
+}
