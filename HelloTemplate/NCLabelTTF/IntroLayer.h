6a201c2c42943f2afb5ea255889d8569e8a84476
hanson <hulefei29@gamil.com>
2013-11-23 00:05:05 +0800

    add HelloTemplate

---
+++ HelloTemplate/NCLabelTTF/IntroLayer.h
@@ -0,0 +1,19 @@
+//
+//  IntroLayer.h
+//  NCLabelTTF
+//
+//  Created by Hanson on 13-2-18.
+//  Copyright __MyCompanyName__ 2013年. All rights reserved.
+//
+
+
+// When you import this file, you import all the cocos2d classes
+#import "cocos2d.h"
+
+// HelloWorldLayer
+@interface IntroLayer : CCScene
+{
+}
+
+
+@end
---
+++ HelloTemplate/cocos2d-2.0/cocos2d_2_0.h
@@ -0,0 +1,13 @@
+//
+//  cocos2d_2_0.h
+//  cocos2d-2.0
+//
+//  Created by Hanson on 13-2-18.
+//
+//
+
+#import <Foundation/Foundation.h>
+
+@interface cocos2d_2_0 : NSObject
+
+@end
