2ba08bd79be110ee742eb81afc0df1192860a66e
hanson <hulefei29@gamil.com>
2013-12-15 16:24:44 +0800

    modify

--- HelloOpengles/HelloOpengles/AppDelegate.h
+++ HelloOpengles/HelloOpengles/AppDelegate.h
@@ -7,8 +7,40 @@
 //
 
 #import <UIKit/UIKit.h>
+#import <GLKit/GLKit.h>
 
-@interface AppDelegate : UIResponder <UIApplicationDelegate>
+typedef struct {
+    GLfloat x;
+    GLfloat y;
+    GLfloat z;
+} Vertex3D;
+
+typedef struct {
+    Vertex3D v1;
+    Vertex3D v2;
+    Vertex3D v3;
+} Triangle3D;
+
+static inline Vertex3D Vertex3DMake(CGFloat inX, CGFloat inY, CGFloat inZ) {
+    Vertex3D ret;
+    ret.x = inX;
+    ret.y = inY;
+    ret.z = inZ;
+    return ret;
+}
+
+static inline Triangle3D Triangle3DMake(Vertex3D vertex1, Vertex3D vertex2, Vertex3D vertex3) {
+    Triangle3D ret;
+    ret.v1 = vertex1;
+    ret.v2 = vertex2;
+    ret.v3 = vertex3;
+    return ret;
+}
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate, GLKViewDelegate> {
+    GLuint _vertexBuffer;
+    GLuint _indexBuffer;
+}
 
 @property (strong, nonatomic) UIWindow *window;
 
--- HelloOpengles/HelloOpengles/AppDelegate.m
+++ HelloOpengles/HelloOpengles/AppDelegate.m
@@ -13,37 +13,55 @@
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
 {
     self.window = [[UIWindow alloc] initWithFrame:[[UIScreen mainScreen] bounds]];
+    
+    
+    EAGLContext * context = [[EAGLContext alloc] initWithAPI:kEAGLRenderingAPIOpenGLES2]; // 1
+    GLKView *view = [[GLKView alloc] initWithFrame:[[UIScreen mainScreen] bounds]]; // 2
+    view.context = context; // 3
+    view.delegate = self; // 4
+    [self.window addSubview:view]; // 5
+    
     // Override point for customization after application launch.
     self.window.backgroundColor = [UIColor whiteColor];
     [self.window makeKeyAndVisible];
     return YES;
 }
 
-- (void)applicationWillResignActive:(UIApplication *)application
-{
-    // Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
-    // Use this method to pause ongoing tasks, disable timers, and throttle down OpenGL ES frame rates. Games should use this method to pause the game.
+- (void)glkView:(GLKView *)view drawInRect:(CGRect)rect {
+        
+    glClearColor(0, 104.0/255.0, 55.0/255.0, 1.0);
+    glClear(GL_COLOR_BUFFER_BIT);
+    
+    
+    glEnable(GL_DEPTH_TEST);
+    glLineWidth(3.0f);
+    glEnable(GL_COLOR_MATERIAL);
+    
+    float axes_length = 0.2;
+    
+    glEnableClientState(GL_VERTEX_ARRAY);
+    glColor4f(1.0, 0, 0, 1);
+    float linevertexs_x[6] = {0,0,0,axes_length,0,0};
+    glVertexPointer(3,GL_FLOAT,0, linevertexs_x);
+    glDrawArrays(GL_LINES,0, 2);
+    
+    glColor4f(0, 1.0, 0, 1);
+    float linevertexs_y[6] = {0,0,0,0,axes_length,0};
+    glVertexPointer(3,GL_FLOAT,0, linevertexs_y);
+    glDrawArrays(GL_LINES,0, 2);
+    
+    
+    glColor4f(0, 0, 1.0, 1);
+    float linevertexs_z[6] = {0,0,0,0,0,axes_length};
+    glVertexPointer(3,GL_FLOAT,0, linevertexs_z);
+    glDrawArrays(GL_LINES,0, 2);
+    
+    //glColor4f(1, 1, 1, 1);
+    
+    glDisableClientState(GL_VERTEX_ARRAY);
+    glDisable(GL_COLOR_MATERIAL);
 }
 
-- (void)applicationDidEnterBackground:(UIApplication *)application
-{
-    // Use this method to release shared resources, save user data, invalidate timers, and store enough application state information to restore your application to its current state in case it is terminated later. 
-    // If your application supports background execution, this method is called instead of applicationWillTerminate: when the user quits.
-}
 
-- (void)applicationWillEnterForeground:(UIApplication *)application
-{
-    // Called as part of the transition from the background to the inactive state; here you can undo many of the changes made on entering the background.
-}
-
-- (void)applicationDidBecomeActive:(UIApplication *)application
-{
-    // Restart any tasks that were paused (or not yet started) while the application was inactive. If the application was previously in the background, optionally refresh the user interface.
-}
-
-- (void)applicationWillTerminate:(UIApplication *)application
-{
-    // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
-}
 
 @end
